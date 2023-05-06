52b427b693022cda3a2f990dd1929f917c8bddd0
Yoonge <7884008@qq.com>
2023-05-06 22:33:13 +0800

    Update.

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-# Message Board SSR
+# Conduit
 
-[Message Board SSR](https://github.com/yoonge/message-board-ssr.git) built with Art-Template + ESlint + Joi + JWT + Koa2 + Prettier + TypeScript.
+[Conduit](https://github.com/yoonge/conduit-ssr.git) - A SSR RealWorld example App built with Art-Template + ESlint + Joi + JWT + Koa2 + Prettier + TypeScript.
 
 ## Getting Started
 
 ```sh
-$ git clone https://github.com/yoonge/message-board-ssr.git
-$ cd message-board-ssr
+$ git clone https://github.com/yoonge/conduit-ssr.git
+$ cd conduit-ssr
 $ pnpm i
 $ pnpm dev
 ```
--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -53,21 +53,6 @@ export default class UserCtrl {
     })
   }
 
-  /**
-   * @api {post} /login Login
-   * @apiVersion 0.1.0
-   * @apiName DoLogin
-   * @apiGroup User
-   *
-   * @apiParam {String} email Email
-   * @apiParam {String} password Password
-   *
-   * @apiSuccess {String} msg Message
-   * @apiSuccess {Object} user Current user's profile
-   *
-   * @apiError UserNotFound   The <code>id</code> of the User was not found.
-   * @apiError (500 Internal Server Error) InternalServerError The server encountered an internal error
-   */
   static async doLogin(ctx: Context, next: Next) {
     try {
       const { email, password } = ctx.request.body as User
@@ -115,53 +100,6 @@ export default class UserCtrl {
     ctx.redirect('/login')
   }
 
-  /**
-   * @api {get} /user/:region/:id/:opt Read data of a User
-   * @apiVersion 0.1.0
-   * @apiName GetCurrentUser
-   * @apiGroup User
-   *
-   * @apiDescription Compare version 0.3.0 with 0.2.0 and you will see the green markers with new items in version 0.3.0 and red markers with removed items since 0.2.0.
-   *
-   * @apiHeader {String} Authorization The token can be generated from your user profile.
-   * @apiHeader {String} X-Apidoc-Cool-Factor=big Some other header with a default value.
-   * @apiHeaderExample {Header} Header-Example
-   *     "Authorization: token 5f048fe"
-   * @apiParam {Number} id User unique ID
-   * @apiParam {String} region=fr-par User region
-   * @apiParam {String} [opt] An optional param
-   *
-   * @apiExample {bash} Curl example
-   * curl -H "Authorization: token 5f048fe" -i https://api.example.com/user/fr-par/4711
-   * curl -H "Authorization: token 5f048fe" -H "X-Apidoc-Cool-Factor: superbig" -i https://api.example.com/user/de-ber/1337/yep
-   * @apiExample {js} Javascript example
-   * const client = AcmeCorpApi('5f048fe');
-   * const user = client.getUser(42);
-   * @apiExample {python} Python example
-   * client = AcmeCorpApi.Client(token="5f048fe")
-   * user = client.get_user(42)
-   *
-   * @apiSuccess {Number}   id            The Users-ID.
-   * @apiSuccess {Date}     registered    Registration Date.
-   * @apiSuccess {String}   name          Fullname of the User.
-   * @apiSuccess {String[]} nicknames     List of Users nicknames (Array of Strings).
-   * @apiSuccess {Object}   profile       Profile data (example for an Object)
-   * @apiSuccess {Number}   profile.age   Users age.
-   * @apiSuccess {String}   profile.image Avatar-Image.
-   * @apiSuccess {Object[]} options       List of Users options (Array of Objects).
-   * @apiSuccess {String}   options.name  Option Name.
-   * @apiSuccess {String}   options.value Option Value.
-   *
-   * @apiError NoAccessRight Only authenticated Admins can access the data.
-   * @apiError UserNotFound   The <code>id</code> of the User was not found.
-   * @apiError (500 Internal Server Error) InternalServerError The server encountered an internal error
-   *
-   * @apiErrorExample Response (example):
-   *     HTTP/1.1 401 Not Authenticated
-   *     {
-   *       "error": "NoAccessRight"
-   *     }
-   */
   static async getCurrentUser(ctx: Context, next: Next) {
     try {
       const cuid = ctx.cookies.get('cuid')
